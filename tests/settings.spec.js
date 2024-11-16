8bbd430a6a000cd07f78f9c50e807bdaf5eee5c5
Julia Yerman <74793435+yermanjulia@users.noreply.github.com>
2024-11-15 21:49:53 -0800

    Updated Setting Page Tests

--- tests/settings.spec.js
+++ tests/settings.spec.js
@@ -8,7 +8,7 @@ test.describe("Settings Page Tests", () => {
   let settingsPage;
   let newUser;
 
-  // Precondition: User is logged in and navigated to the Settings page before each test
+  // Precondition: User is logged in
   test.beforeEach(async ({ page }) => {
     newUser = {
       bio: faker.music.genre(),
@@ -27,63 +27,73 @@ test.describe("Settings Page Tests", () => {
     await mainPage.goToRegister();
     await registerPage.register(newUser.name, newUser.email, newUser.password);
     await registerPage.signupButton.click();
+    // await page.goto(`${url}/#/settings`);
 
     // Assert that the user is redirected to the home page after registration
     await expect(page).toHaveURL("https://realworld.qa.guru/#/");
-
-    // Navigate to the Settings page
     await page.goto(`${url}/#/settings`);
     await expect(page).toHaveURL(`${url}/#/settings`, { timeout: 10000 });
   });
-  test("Settings Page Tests", async ({ page }) => {
-    // Test: User can navigate to the Settings page
-    await test.step("User can navigate to the Settings page", async () => {
-      await expect(page).toHaveURL(`${url}/#/settings`);
-    });
 
-    // Test: User can insert URL of profile picture
-    await test.step("User can insert URL of profile picture", async () => {
-      const urlProfilePicture = faker.image.url();
+  // test("Settings Page Tests", async ({ page }) => {
+  //   // Test: User can navigate to the Settings page
+  //   await test.step("User can navigate to the Settings page", async () => {
+  //     await expect(page).toHaveURL(`${url}/#/settings`);
+  //   });
 
-      await settingsPage.insertUrlPicture(urlProfilePicture);
-      // Check if the profile picture is set to the expected URL
-      await expect(settingsPage.profilePicture).toHaveAttribute(
-        "value",
-        urlProfilePicture
-      );
-    });
+  //User can insert URL of profile picture
+  test("User can insert URL of profile picture", async () => {
+    const urlProfilePicture = faker.image.url();
 
-    // Test: User can update name
-    await test.step("User can update name", async () => {
-      const name = faker.person.firstName();
-      await settingsPage.updateName(name);
-      await expect(settingsPage.nameField).toHaveValue(name);
-    });
+    await settingsPage.insertUrlPicture(urlProfilePicture);
+    await settingsPage.updateSettings();
 
-    // Test: User can enter bio
-    await test.step("User can enter bio", async () => {
-      const bio = faker.music.genre();
-      await settingsPage.enterUserBio(bio);
-      await expect(settingsPage.bioField).toHaveValue(bio);
-    });
+    await expect(settingsPage.profilePicture).toHaveAttribute(
+      "value",
+      urlProfilePicture
+    );
+  });
 
-    // Test: User can update email
-    await test.step("User can update email", async () => {
-      const email = faker.internet.email();
-      await settingsPage.updateEmail(email);
-      await expect(settingsPage.emailField).toHaveValue(email);
-    });
+  // Test: User can update name
 
-    // Test: User can update password
-    await test.step("User can update password", async () => {
-      const password = faker.internet.password();
-      await settingsPage.updatePassword(password);
-    });
+  test("User can update name", async () => {
+    const name = faker.person.firstName();
+    await settingsPage.updateName(name);
+    await settingsPage.updateSettings();
 
-    // Test: User can update settings
-    await test.step("User can update settings", async () => {
-      await settingsPage.updateSettings();
-      await expect(await settingsPage.updateSettingsButton).toBeHidden();
-    });
+    await expect(settingsPage.nameField).toHaveValue(name);
+  });
+
+  //   // Test: User can enter bio
+  test("User can enter bio", async () => {
+    const bio = faker.music.genre();
+    await settingsPage.enterUserBio(bio);
+    await settingsPage.updateSettings();
+
+    await expect(settingsPage.bioField).toHaveValue(bio);
+  });
+
+  // Test: User can update email
+  test("User can update email", async () => {
+    const email = faker.internet.email();
+
+    await settingsPage.updateEmail(email);
+    await settingsPage.updateSettings();
+
+    await expect(settingsPage.emailField).toHaveValue(email);
+  });
+
+  // Test: User can update password
+
+  test("User can update password", async () => {
+    const password = faker.internet.password();
+
+    await settingsPage.updatePassword(password);
+  });
+
+  test("User is able to login with updated password", async () => {
+    await settingsPage.logout();
+    await settingsPage.login(newUser.email, newPassword);
+    await expect(page).toHaveURL(`${url}/#/settings`, { timeout: 10000 });
   });
 });
